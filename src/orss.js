1edb0ee0998ff625ef021bb861f6bb47f5dd849b
Rawley Fowler <rawleyfowler@gmail.com>
2022-10-05 07:05:21 -0600

    split up index

--- src/orss.js
+++ src/orss.js
@@ -1,14 +1,11 @@
 const path = require('path')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const { body, validationResult } = require('express-validator')
 const cookieSession = require('cookie-session')
-const prettyPrintError = require('./utils/ppError')
 const express = require('express')
 const app = express()
 
-const postRepository = require('./db/repos/post')
-const siteRepository = require('./db/repos/site')
+const indexRouter = require('./routes/index')
 const crawler = require('./services/crawler')
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -28,6 +25,8 @@ if (process.env.NODE_ENV === 'dev') {
   }))
 }
 
+app.use('/', indexRouter)
+
 app.use(cookieSession({
   name: 'orss_session',
   path: '/*',
@@ -37,33 +36,11 @@ app.use(cookieSession({
   httpOnly: true
 }))
 
-const getSitesWithPosts = () =>
-  siteRepository
-    .findAll()
-    .map(site => ({...site, posts: postRepository.findAllBySiteUri(site.uri)}))
-
-
-app.get('/', (req, res) => {
+// Start the crawler
+setTimeout(() => {
+  console.log('Crawler started')
   crawler.crawl()
-
-  return res.render('index', { sites: getSitesWithPosts() })
-})
-
-app.post('/', 
-  body('rss').isString().isURL(),
-  (req, res) => {
-  const errors = validationResult(req).errors.map(prettyPrintError)
-
-  if (errors.length) {
-    return res.render('index', { errors })
-  }
-
-  if(!siteRepository.insert(req.body.rss)) {
-    return res.render('index', { errors: ['Something went wrong saving that URI!'] })
-  }
-
-  return res.render('index', { sites: getSitesWithPosts })
-})
+}, 500000)
 
 module.exports = () =>
   app.listen(ORSS_PORT, () => console.log(`Listening on port ${ORSS_PORT}, http://localhost:${ORSS_PORT}`))
---
+++ src/routes/index.js
@@ -0,0 +1,36 @@
+const router = require('express').Router()
+const { body, validationResult } = require('express-validator')
+
+const prettyPrintError = require('../utils/ppError')
+const crawler = require('../services/crawler')
+const postRepository = require('../db/repos/post')
+const siteRepository = require('../db/repos/site')
+
+const getSitesWithPosts = () =>
+  siteRepository
+    .findAll()
+    .map(site => ({...site, posts: postRepository.findAllBySiteUri(site.uri)}))
+
+router.get('/', (req, res) => {
+  crawler.crawl()
+  return res.render('index', { sites: getSitesWithPosts() })
+})
+
+router.post('/', 
+  body('rss').isString().isURL(),
+  (req, res) => {
+  const errors = validationResult(req).errors.map(prettyPrintError)
+
+  if (errors.length) {
+    return res.render('index', { errors })
+  }
+
+  if(!siteRepository.insert(req.body.rss)) {
+    return res.render('index', { errors: ['Something went wrong saving that URI!'] })
+  }
+
+  return res.render('index', { sites: getSitesWithPosts })
+})
+
+
+module.exports = router
